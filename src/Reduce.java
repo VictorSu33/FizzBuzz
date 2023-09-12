dd1d57e60f5a47afa73d26243d4e824b6355a8dd
ProudScottsTot <87554004+ProudScottsTot@users.noreply.github.com>
2023-09-12 12:54:34 -0400

    Added main.java file

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Reduce {
+import java.util.Scanner;
+public class Reduce {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt();
+        reader.close();
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n/2;
+            }
+            else {
+                n -= 1;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
 }
